f5b23c9ac0e6cc32c1000d399947a85a3f9c2493
Anusha Ranganathan <anusha@cottagelabs.com>
2022-03-24 12:30:06 +0000

    Generated spotlight application

--- spotlight/config/routes.rb
+++ spotlight/config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  mount Blacklight::Oembed::Engine, at: 'oembed'
+  root to: 'spotlight/exhibits#index'
+  # root to: "catalog#index" # replaced by spotlight root path
+  devise_for :users
+  mount Riiif::Engine => '/images', as: 'riiif'
+  mount Spotlight::Engine, at: 'spotlight'
+  mount Blacklight::Engine => '/'
+
+  concern :searchable, Blacklight::Routes::Searchable.new
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  concern :exportable, Blacklight::Routes::Exportable.new
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
