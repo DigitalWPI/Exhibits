4329abb4c39defc33d8c76aeb2b4ea68747d37a7
Anusha Ranganathan <anusha@cottagelabs.com>
2022-04-07 12:27:49 +0100

    Modified thumbnail url

--- spotlight/app/helpers/application_helper.rb
+++ spotlight/app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
   include SpotlightHelper
+  def render_thumbnail(document, options)
+    image_tag(
+      "https://digital.wpi.edu/downloads/#{document.id}?file=thumbnail", options)
+  end
 end
