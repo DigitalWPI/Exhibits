655ce5ba64d6586d11445ff774c9c6830f0d4d1d
Anusha Ranganathan <anusha@cottagelabs.com>
2022-06-01 20:03:21 +0100

    Added echo statements to assist with debugging

--- spotlight/docker-entrypoint.sh
+++ spotlight/docker-entrypoint.sh
@@ -4,6 +4,7 @@ echo "current env $RAILS_ENV"
 echo "Creating log folder"
 mkdir -p $APP_WORKDIR/log
 
+echo "check bundle"
 if [ "$RAILS_ENV" = "production" ]; then
     # Verify all the production gems are installed
     bundle check
@@ -14,6 +15,7 @@ fi
 
 ## Run any pending migrations, if the database exists
 ## If not setup the database
+echo "setup db"
 bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
 
 # setup test db for unit test
@@ -27,6 +29,6 @@ fi
 # bundle exec rake spotlight:setup_hyrax["seed/setup.json"]
 npm install --unsafe-perm  # install uv, --unsafe-perm for root permission
 
-# echo "--------- Starting Hyrax in $RAILS_ENV mode ---------"
+echo "--------- Starting Spotlight in $RAILS_ENV mode ---------"
 rm -f /tmp/spotlight.pid
 bundle exec rails server -p 3005 -b '0.0.0.0' --pid /tmp/spotlight.pid
